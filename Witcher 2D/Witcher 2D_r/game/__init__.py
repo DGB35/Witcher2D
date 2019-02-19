50f7e8d9eed40039a29ed6433d1bb8d5084fc9f0
DGB35 <39164397+DGB35@users.noreply.github.com>
2019-02-19 17:03:58 +0200

    Initial commit

---
+++ Witcher 2D/Witcher 2D_r/game/__init__.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+screen_size = width, height = 640, 480
+gravity = 0.0005
+
+class Colors(Enum):
+    black = (0, 0, 0)
+    white = (255, 255, 255)
---
+++ Witcher 2D/Witcher 2D_r/game/game.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+import time
+from . import Colors, screen_size
+from .objects.hero import Hero
+from .objects.platform import Platform
+from .objects.position import Position
+from pygame import key
+
+def start():
+    pygame.init()
+
+    screen = pygame.display.set_mode(screen_size)
+    hero = Hero()
+    platforms = []
+    for i in range(10):
+        platforms.append(Platform(Position(x=0+i*50, y=300)))
+    to_draw = [*platforms, hero]
+
+    cur_time = time.time()
+    time_delta = 0
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        keys = key.get_pressed()
+
+        time_delta = time.time()-cur_time
+        cur_time = time.time()
+        hero.update_pos(keys, platforms, time_delta)
+        hero.update_anim(time_delta)
+
+        screen.fill(Colors.black.value)
+        for item in to_draw:
+            item.put_on_screen(screen)
+        pygame.display.flip()
